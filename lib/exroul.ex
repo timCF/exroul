a5421bad0b27a1ae79fd59a859e96cbf7827212b
tim <llStrainll@gmail.com>
2016-02-25 22:28:59 +0300

    add &valid/1 func

--- lib/exroul.ex
+++ lib/exroul.ex
@@ -16,4 +16,36 @@ defmodule Exroul do
 		opts = [strategy: :one_for_one, name: Exroul.Supervisor]
 		Supervisor.start_link(children, opts)
 	end
+
+	#
+	#	balls - dicts mandatory unique field : value (int)
+	#	zeros - unique ints
+	#
+
+	defmacro __using__([balls: balls = [_|_], zeros: zeros = [_|_]]) do
+		prop_keys = List.first(balls) |> Dict.keys |> Enum.sort
+		prop_vals = Stream.flat_map(balls, fn(ball) -> Enum.map(prop_keys, &(ball[&1])) end) |> Enum.uniq
+		values = Enum.map(balls, &(&1[:value]))++zeros
+		true = Enum.all?(balls, &(prop_keys == (&1 |> Dict.keys |> Enum.sort)))
+		true = Enum.all?(values, &is_integer/1)
+		true = (length(balls++zeros) == (values |> Enum.uniq |> length))
+		base_odd = length(balls)
+		properties_oddsmap = Enum.reduce(prop_keys, %{}, fn(prop, acc) ->
+			#
+			#	TODO
+			#
+		end)
+		quote location: :keep do
+			def valid?(subj) when (subj in unquote(prop_vals)), do: true
+			def valid?(subj = [_|_]) do
+				Enum.all?(subj, &(&1 in unquote(values)))
+				and
+				(length(subj) == (subj |> Enum.uniq |> length))
+				and
+				(length(subj) in [1,2,3,4,6])
+			end
+			def valid?(_), do: false
+		end
+	end
+
 end
