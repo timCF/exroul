08c16382347621f8ba990632c45be760a797ea7a
tim <llStrainll@gmail.com>
2016-02-26 18:56:55 +0300

    add win func

--- lib/exroul.ex
+++ lib/exroul.ex
@@ -17,35 +17,85 @@ defmodule Exroul do
 		Supervisor.start_link(children, opts)
 	end
 
+
 	#
 	#	balls - dicts mandatory unique field : value (int)
 	#	zeros - unique ints
 	#
 
-	defmacro __using__([balls: balls = [_|_], zeros: zeros = [_|_]]) do
-		prop_keys = List.first(balls) |> Dict.keys |> Enum.sort
+
+	defp make_combos_win(combos_odds) do
+		Enum.reduce(combos_odds, nil, fn
+			{len, odd}, nil ->
+				make_combos_win_process(len, odd)
+			{len, odd}, acc ->
+				quote location: :keep do
+					unquote(acc)
+					unquote(make_combos_win_process(len, odd))
+				end
+		end)
+	end
+	defp make_combos_win_process(len, odd) do
+		quote location: :keep do
+			def win(n, bet = [_|_]) when (length(bet) == unquote(len)) do
+				case Enum.member?(n, bet) do
+					true -> unquote(odd)
+					false -> 0
+				end
+			end
+		end
+	end
+
+
+	defp make_props_win(balls, props_odds) do
+		#
+		#	TODO
+		#
+		nil
+	end
+
+
+
+	defmacro __using__([balls: balls = [_|_], zeros: zeros = [_|_], combos: combos = [_|_]]) do
+		prop_keys = List.first(balls) |> Dict.keys |> Stream.filter(&(&1 != :value)) |> Enum.sort
 		prop_vals = Stream.flat_map(balls, fn(ball) -> Enum.map(prop_keys, &(ball[&1])) end) |> Enum.uniq
 		values = Enum.map(balls, &(&1[:value]))++zeros
-		true = Enum.all?(balls, &(prop_keys == (&1 |> Dict.keys |> Enum.sort)))
+		true = Enum.all?(balls, fn(ball) -> prop_keys == (ball |> Dict.keys |> Stream.filter(&(&1 != :value)) |> Enum.sort) end)
 		true = Enum.all?(values, &is_integer/1)
 		true = (length(balls++zeros) == (values |> Enum.uniq |> length))
 		base_odd = length(balls)
-		properties_oddsmap = Enum.reduce(prop_keys, %{}, fn(prop, acc) ->
-			#
-			#	TODO
-			#
+		true = Enum.all?(combos, &(is_integer(&1) and (&1 > 0) and (&1 < base_odd)))
+		combos_odds = Enum.reduce(combos, %{}, fn(n, acc) ->
+			raw = base_odd / n
+			res = round(raw)
+			true = ((raw == res) and is_integer(res) and (res > 1))
+			Map.put(acc, n, res)
 		end)
-		quote location: :keep do
+		props_odds = Enum.reduce(prop_keys, %{}, fn(prop_key, acc) ->
+			Enum.group_by(balls, &(&1[prop_key]))
+			|> Enum.reduce(acc, fn({prop_val, balls = [_|_]}, acc) ->
+				this_odd = base_odd / length(balls)
+				case round(this_odd) do
+					odd when (is_integer(odd) and (odd > 1) and (odd == this_odd)) -> Map.put(acc, prop_val, odd)
+					_ -> raise("got wrong odd #{inspect this_odd}")
+				end
+			end)
+		end)
+		res = quote location: :keep do
 			def valid?(subj) when (subj in unquote(prop_vals)), do: true
 			def valid?(subj = [_|_]) do
 				Enum.all?(subj, &(&1 in unquote(values)))
 				and
 				(length(subj) == (subj |> Enum.uniq |> length))
 				and
-				(length(subj) in [1,2,3,4,6])
+				(length(subj) in unquote(combos))
 			end
 			def valid?(_), do: false
+			unquote(make_combos_win(combos_odds))
+			unquote(make_props_win(balls, props_odds))
 		end
+		Macro.to_string(res) |> IO.puts
+		res
 	end
 
 end
--- test/exroul_test.exs
+++ test/exroul_test.exs
@@ -1,8 +1,18 @@
-defmodule ExroulTest do
-  use ExUnit.Case
-  doctest Exroul
+defmodule ExroulTest.Macro do
+	defmacro __using__([]) do
+		balls = Enum.map(1..36, fn(n) -> [value: n, color: (case rem(n,2) do ; 0 -> :red ; 1 -> :black ; end)] end)
+		quote location: :keep do
+			use Exroul, [balls: unquote(balls), zeros: [0], combos: [1,2,3,4,6]]
+		end
+	end
+end
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+defmodule ExroulTest do
+	use ExUnit.Case
+	doctest Exroul
+	use ExroulTest.Macro
+
+	test "the truth" do
+		assert 1 + 1 == 2
+	end
 end
